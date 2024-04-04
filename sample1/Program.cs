a24f66d16703ae7176adc7884c00e3ebe10a9971
Rigaud Jules <jules.rigaud@student-cs.fr>
2024-04-05 00:00:12 +0200

    Partie 1 - Hello World

---
+++ sample1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace sample1;
+
+public class Personne
+{
+    public string? Nom { get; set; }
+    public int Age { get; set; }
+
+    public string Hello(bool isLowercase)
+    {
+        string raw = $"hello {this.Nom}, you are {this.Age}";
+
+        if (isLowercase) {
+            return raw.ToLower();
+        } else {
+            return raw.ToUpper();
+        }
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var james = new Personne
+        {
+            Nom = "James",
+            Age = 43
+        };
+
+        Console.WriteLine(james.Hello(isLowercase: true));
+
+    }
+}
