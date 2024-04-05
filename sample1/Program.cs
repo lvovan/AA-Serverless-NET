6d7d0699b2eaba5e6fe432617c985436d8461183
Rigaud Jules <jules.rigaud@student-cs.fr>
2024-04-05 13:40:48 +0200

    Partie 3 - Traitement d'image locale
    - redimensionnement d'une image en local : robot.jpeg passe de 213 Ko à 63 Ko (environ divisée par 4 comme prévu)
    
    - application d'un effet à un groupe de 8 images (dans img/benchmark/) avec un ForEach classique et un ForEach parallèle :
    Classical foreach loop | Time Taken : 5565 ms.
    Parallel.ForEach loop  | Time Taken : 4630 ms.

--- sample1/Program.cs
+++ sample1/Program.cs
@@ -1,6 +1,62 @@
 ﻿namespace sample1;
 
-using Newtonsoft.Json;
+// using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // var james = new Personne
+        // {
+        //     Nom = "James",
+        //     Age = 43
+        // };
+
+        // string serialized = JsonConvert.SerializeObject(james, formatting: Formatting.Indented);
+        // Console.WriteLine(serialized);
+        using (Image image = Image.Load(@"img\robot.jpeg"))
+        {
+            image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+
+            image.Save(@"img\robot-resized.jpeg");
+        }
+
+        // Test de la fonction OilPaintImage
+        OilPaintImage(@"img\robot.jpeg");
+
+        // Construction de la liste des fichiers à benchmarker
+        string[] fileNames = Directory.GetFiles(@"img/benchmark");
+
+        var watch = Stopwatch.StartNew();
+        Array.ForEach(fileNames, OilPaintImage);
+        watch.Stop();
+
+        var watchForParallel = Stopwatch.StartNew();
+        Parallel.ForEach(fileNames, OilPaintImage);
+        watchForParallel.Stop();
+
+        Console.WriteLine($"Classical foreach loop | Time Taken : {watch.ElapsedMilliseconds} ms.");
+        Console.WriteLine($"Parallel.ForEach loop  | Time Taken : {watchForParallel.ElapsedMilliseconds} ms.");
+
+
+    }
+
+    static void OilPaintImage(string path)
+    {
+        using (Image image = Image.Load(path))
+        {
+            image.Mutate(x => x.OilPaint());
+            string outputPath = $"{Path.GetDirectoryName(path)}\\{Path.GetFileNameWithoutExtension(path)}_oil{Path.GetExtension(path)}";
+            image.Save(outputPath);
+        }
+    }
+}
 
 public class Personne
 {
@@ -18,19 +74,4 @@ public class Personne
         }
     }
 
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var james = new Personne
-        {
-            Nom = "James",
-            Age = 43
-        };
-
-        string serialized = JsonConvert.SerializeObject(james, formatting: Formatting.Indented);
-        Console.WriteLine(serialized);
-    }
 }
